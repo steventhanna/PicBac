1f079ca0c443d63046314e69ee73ff39e5267de8
steventhanna <steventhanna@gmail.com>
2015-12-26 14:06:12 -0600

    File copying works correctly

--- PicBac.py
+++ PicBac.py
@@ -6,6 +6,9 @@ source = "/Users/steventhanna/desktop/work"
 # The destination drive to copy
 destination = "/Users/steventhanna/desktop/testing"
 
+# File counter
+fileCounter = 0;
+
 
 def main():
 
@@ -22,31 +25,51 @@ def main():
 
 def walkAndCopy(folder):
     dirs = os.listdir(folder)
-    for file in dirs:
-        print file
-        print isPicture(file)
-        if os.path.isdir(file) == True:
-            walkAndCopy(file)
+    for fileName in dirs:
+        if os.path.isdir(folder + "/" + fileName) == True:
+            walkAndCopy(folder + "/" + fileName)
         else:
-            print os.path.abspath(file)
-            if isPicture(file) == True:
-                shutil.copyfile(file, destination)
+            if isPicture(folder + "/" + fileName) == True:
+                copyFile(folder + "/" + fileName, destination + "/" + fileName)
 
+def copyFile(src, dest):
+    # Check if the file already exists
+    if os.path.isfile(dest) == True:
+        # Ask the user what to do
+        fileArr = dest.split("/")
+        print "The file: " + fileArr[len(fileArr) - 1] + " already has been copied onto the drive."
+        print "1. Replace File 2. Keep Both Files"
+        response = raw_input()
+        if response == "1":
+            shutil.copyfile(src, dest)
+            global fileCounter
+            fileCounter += 1
+            print "Amount of Files Copied: ", fileCounter
+        elif response == "2":
+            copyFile(src, dest + "_copy")
+        else:
+            print "Invalid input."
+            copyFile(src, dest)
+    else:
+        shutil.copyfile(src, dest)
+        global fileCounter
+        fileCounter += 1
+        print "Amount of Files Copied: ", fileCounter
 
 def isPicture(fileName):
     if os.path.isdir(os.path.abspath(fileName)) == True:
         return False
-    extensions = {"png", "jpg", "jpeg", "gif", "tiff"}
-    extend = os.path.abspath(fileName)
-    extendArr = extend.split("/")
-    finalName = extendArr[len(extendArr) - 1]
-    extendArr2 = finalName.split(".")
-    extendFinal = extendArr2[len(extendArr2) - 1].lower()
-    print "EXTEND: " + extendFinal
-    for fileType in extensions:
-        if(fileType == extendFinal):
-            return True
-    return False
+    else:
+        extensions = {"png", "jpg", "jpeg", "gif", "tiff"}
+        extend = os.path.abspath(fileName)
+        extendArr = extend.split("/")
+        finalName = extendArr[len(extendArr) - 1]
+        extendArr2 = finalName.split(".")
+        extendFinal = extendArr2[len(extendArr2) - 1].lower()
+        for fileType in extensions:
+            if(fileType == extendFinal):
+                return True
+        return False
 
 if __name__ == "__main__":
     main()
